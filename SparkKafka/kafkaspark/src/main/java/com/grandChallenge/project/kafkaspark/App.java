177bd6aa6411f1ba2925d6666966059cf355fef7
Milan Bhandari <milanbhandari2013@gmail.com>
2019-10-22 15:01:33 -0400

    New project added

---
+++ SparkKafka/kafkaspark/src/main/java/com/grandChallenge/project/kafkaspark/App.java
@@ -0,0 +1,13 @@
+package com.grandChallenge.project.kafkaspark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
